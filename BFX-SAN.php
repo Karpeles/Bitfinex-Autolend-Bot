9dec0e8dc9b5f1b89b2bf97d65d27e7392f70b30
Dark Karpeles <35708049+Karpeles@users.noreply.github.com>
2018-01-23 01:00:41 +0000

    Update BFX-SAN.php

--- BFX-SAN.php
+++ BFX-SAN.php
@@ -7,7 +7,7 @@ $a->apiKey = '_________________________________________';			# Your API key, obta
 $a->apiSecret = '_________________________________________';		# Your API secret, obtained from the Bitfinex site
 $a->tries = 5;														# Number of times to retry API requests that fail
 $a->currency = 'san';												# Currencey to lend out (usd, eth, btc...)
-$a->placeAboveLowestAsk = 100000;									# Number of units to offer lending above the lowest ask, e.g. if the lowest ask is for 0.05 and placeAboveLowestAsk = 1,000,000 the bot will lend out at a rate where there are 1,000,000 units offered above the lowest ask
+$a->placeAboveLowestAsk = 5000;									# Number of units to offer lending above the lowest ask, e.g. if the lowest ask is for 0.05 and placeAboveLowestAsk = 1,000,000 the bot will lend out at a rate where there are 1,000,000 units offered above the lowest ask
 $a->minimumRate = 0.01;												# Rate to lend at will be set to this if calculated offer rate is below it
 $a->periodSchedule = array(											# Number of days to lend by rate, e.g.
 	'0.04' => 2,													# 0.05 => 3 - all lending between 0.05 and 0.059r will be for 3 days
